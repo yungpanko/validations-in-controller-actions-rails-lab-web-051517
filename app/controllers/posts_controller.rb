82d88473140383c2715ebb47ec043d9bff658265
yungpanko <jrjohns@mit.edu>
2017-06-13 10:44:21 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,12 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
   end
 
   private
